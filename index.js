9b9b30b153946e182c6dd52ee16ef59c965f10f3
bataevvlad <bataevvlad@gmail.com>
2024-10-17 14:08:48 +0200

    Updates 2

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const submodule1 = require('./submodule-repo-1/math');
 const submodule2 = require('./submodule-repo-2/math');
+const subtree = require('./subtree-repo/math');
 
-console.log("Basic Operations:");
+console.log("-----------------------");
+console.log("Submodule 1 Operations:");
 console.log("Addition (5 + 3):", submodule1.add(5, 3));
 console.log("Subtraction (10 - 7):", submodule1.subtract(10, 7));
 console.log("Multiply (10 * 7):", submodule1.multiply(10, 7));
-
-console.log("Advanced Operations:");
+console.log("-----------------------");
+console.log("Submodule 2 Operations:");
 console.log("Factorial (5):", submodule2.factorial(5));
 console.log("Is Prime (11):", submodule2.isPrime(11));
+console.log("-----------------------");
+console.log("Subtree repo:");
+console.log("Divide (5):", subtree.divide(10, 2));
+console.log("-----------------------");
